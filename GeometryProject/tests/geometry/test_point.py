54997cb357fefc240e6f552ee15565b515227724
matthcol <matthias.colin@gmail.com>
2025-03-18 14:01:46 +0100

    unit test distance

--- GeometryProject/tests/geometry/test_point.py
+++ GeometryProject/tests/geometry/test_point.py
@@ -5,6 +5,8 @@ from geometry import Point
 def pointA():
     return Point(x=3.5, y=7.25)
 
+
+# this test modify the fixture without impact on another tests
 def test_set_x(pointA):
     pointA.x = 4.125
     assert 4.125 == pointA.x
@@ -15,3 +17,26 @@ def test_constructor(pointA):
 
 def test_distance_same(pointA):
     assert 0.0 == pointA.distance(pointA)
+
+@pytest.mark.parametrize(
+        "x1, y1, x2, y2, expected_distance, epsilon",
+        [
+            (4.25, 5.125, 1.25, 9.125, 5.0, 0.0),
+            (4.25E-150, 5.125E-150, 1.25E-150, 9.125E-150, 5E-150, 0.0),
+            (4.25E-300, 5.125E-300, 1.25E-300, 9.125E-300, 5E-300, 0.0),
+            (4.25E+150, 5.125E+150, 1.25E+150, 9.125E+150, 5E+150, 0.0),
+            (4.25E+300, 5.125E+300, 1.25E+300, 9.125E+300, 5E+300, 0.0),
+        ],
+        ids=[
+            "triangle 345",
+            "triangle 345 scale 1E-150",
+            "triangle 345 scale 1E-300",
+            "triangle 345 scale 1E+150",
+            "triangle 345 scale 1E+300",
+        ]
+)
+def test_distance_different(x1, y1, x2, y2, expected_distance, epsilon):
+    p1 = Point(x=x1, y=y1)
+    p2 = Point(x=x2, y=y2)
+    actual_distance = p1.distance(p2)
+    assert expected_distance == actual_distance # +/- epsilon
